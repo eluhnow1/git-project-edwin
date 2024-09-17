938eec5066803ac4cb28fa13029f369e925aa078
Matthew <mcommons1@hwemail.com>
2024-09-17 13:32:29 -0700

    Started writing git initialize method

--- Git.java
+++ Git.java
@@ -1,5 +1,12 @@
+import java.io.File;
+
 public class Git {
     public static void main (String [] args) {
+        
+    }
+    public static void initializeRepo (String repoName){
+        File repo = new File (repoName);
+        repo.mkdir();
         
     }
 }
